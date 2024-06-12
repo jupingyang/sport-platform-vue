619c9b62cea950a03cac9cb5f6432ee5d23e019f
ping <jupingyang0@gmail.com>
2024-06-12 17:15:57 +0800

    新增updateUser功能

--- src/components/admin/UserList.vue
+++ src/components/admin/UserList.vue
@@ -42,7 +42,7 @@
                 <el-table-column label="操作">
                     <template #default="scope">
                         <!-- 修改按鈕 -->
-                        <el-button type="primary" size="small">
+                        <el-button type="primary" size="small" @click="showUpdateDialog(scope.row.id)">
                             <el-icon ><edit /></el-icon> 
                         </el-button>
                         <!-- 刪除按鈕 -->
@@ -51,8 +51,8 @@
                             <el-icon ><delete /></el-icon>  
                         </el-button>
                         <!-- 設定按鈕 -->
-                        <el-tooltip effect="dark" content="權限管理" placement="top-start" :enterable="false"><!-- 文字提示 enterable 隱藏-->
-                            <el-button type="warning" size="small" >
+                        <el-tooltip effect="dark" content="修改權限" placement="top-start" :enterable="false"><!-- 文字提示 enterable 隱藏-->
+                            <el-button type="warning" size="small" @click="showSettingDialog(scope.row.id)">
                                 <el-icon ><setting /></el-icon> 
                             </el-button>
                         </el-tooltip>
@@ -88,11 +88,53 @@
                     <el-input v-model="addForm.email"></el-input>
                 </el-form-item>               
             </el-form>
-            <!-- 內容底部區域 --><!-- 看作用域插槽 <template #default="scope">-->  
+            <!-- 內容底部區域 --><!-- 作用域插槽 <template #default="scope">-->  
             <template #footer>          
                 <span class="dialog-footer">                           
                     <el-button @click="addDialogVisible = false">取消</el-button>    
                     <el-button type="primary" @click="addUser">確定</el-button>                         
+                </span>
+            </template>
+        </el-dialog>
+        
+        <!-- 更新用戶對話框 -->
+        <el-dialog title="更新用戶" v-model="updateDialogVisible" width="50%" @close="updateDialogClosed">
+            <el-form :model="updateForm" :rules="updateFormRules" ref="updateFormRef" label-width="70px" >
+                <el-form-item label="用戶名" prop="username">
+                    <el-input v-model="updateForm.username" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="密碼" prop="password">
+                    <el-input v-model="updateForm.password"></el-input>
+                </el-form-item>
+                <el-form-item label="email" prop="email">
+                    <el-input v-model="updateForm.email"></el-input>
+                </el-form-item>               
+            </el-form>
+            <!-- 內容底部區域 --><!-- 看作用域插槽 <template #default="scope">-->  
+            <template #footer>          
+                <span class="dialog-footer">                           
+                    <el-button @click="updateDialogVisible = false">取消</el-button>    
+                    <el-button type="primary" @click="updateUserInfo">確定</el-button>                         
+                </span>
+            </template>
+        </el-dialog>
+
+        <!-- 設定用戶權限對話框 -->
+        <el-dialog title="設定用戶權限" v-model="settingDialogVisible" width="50%" @close="settingDialogClosed">
+            <el-form :model="settingForm" ref="settingFormRef" label-width="70px" >
+                <el-form-item label="用戶權限" prop="role">
+                    <el-radio-group v-model="settingForm.role" @change="handleChange">
+                        <el-radio :value="0" >超級管理員</el-radio><!--value="admimistrator"-->
+                        <el-radio :value="1" >普通管理員</el-radio><!--value="powerUser"-->
+                        <el-radio :value="2" >普通用戶</el-radio><!--value="user"-->
+                    </el-radio-group>
+                </el-form-item>                  
+            </el-form>
+            <!-- 內容底部區域 -->
+            <template #footer>          
+                <span class="dialog-footer">                           
+                    <el-button @click="settingDialogVisible = false">取消</el-button>    
+                    <el-button type="primary" @click="userRoleChanged">確定</el-button>                         
                 </span>
             </template>
         </el-dialog>
@@ -113,19 +155,45 @@ export default {
                 pageNum: 1,// 當前頁
                 pageSize: 5,// 每頁顯示記錄條數
             },
-            userList:[],// 用戶列表
-            total: 0,// 總記錄數
-            addDialogVisible: false,// 對話框隱藏/顯示狀態             
-            addForm: { // 新增用戶表單項目
+            // 用戶列表
+            userList:[],
+            // 總記錄數
+            total: 0,
+            // 新增用戶對話框隱藏/顯示
+            addDialogVisible: false,            
+            // 新增用戶表單
+            addForm: { 
                 username:'',
                 password:'',
                 email:'',
-            }, 
+            },    
+            // 更新用戶表單
+            updateForm: {}, 
+            // 設定用戶權限表單
+            settingForm: {
+                role:'',
+            },
+            // 更新用戶對話框隱藏/顯示 
+            updateDialogVisible: false,
+            // 設定用戶對話框隱藏/顯示 
+            settingDialogVisible: false,
+            // 新增用戶表單驗證   
             addFormRules:{
                 username:[
                     { required: true, message: "請輸入用戶名稱", trigger: "blur" },
                     { min: 6, max: 8, message: "長度在 6 到 8 個字符間", trigger: "blur" }
                 ],
+                password:[
+                    { required: true, message: "請輸入密碼", trigger: "blur" },
+                    { min: 6, max: 8, message: "長度在 6 到 8 個字符間", trigger: "blur" }
+                ],
+                email:[
+                    { required: true, message: "請輸入電子信箱", trigger: "blur" },
+                    { min: 6, max: 30, message: "請輸入正確電子信箱地址", trigger: "blur" }
+                ],
+            },
+            // 更新用戶表單驗證
+            updateFormRules:{
                 password:[
                     { required: true, message: "請輸入密碼", trigger: "blur" },
                     { min: 6, max: 8, message: "長度在 6 到 8 個字符間", trigger: "blur" }
@@ -168,7 +236,7 @@ export default {
             } 
             this.$message.success("操作成功!");
         },
-        // 監聽新增用戶
+        // 關閉新增對話框, 表單內容重置
         addDialogClosed(){
             this.$refs.addFormRef.resetFields();// 表單內容重置
         },
@@ -204,8 +272,82 @@ export default {
             this.$message.success("刪除成功!");
             this.getUserList();
         },
-    },    
-}
+        // 顯示更新用戶對話框
+        async showUpdateDialog(id){
+            const {data:res} = await this.$axios.get("http://localhost:9000/getUser?id="+id);            
+            this.updateForm = res; // 查出的用戶資料反填到updateForm
+            this.updateDialogVisible = true; // 開啟更新對話框           
+        },
+        // 關閉更新對話框, 表單內容重置
+        updateDialogClosed(){
+            this.$refs.updateFormRef.resetFields();
+        },
+        // 內容更新確認
+        updateUserInfo(){
+            this.$refs.updateFormRef.validate(async valid =>{
+                if(!valid) return;
+                // 發更新請求
+                const {data:res} = await this.$axios.put("http://localhost:9000/updateUser", this.updateForm);
+                if( res != "success"){              
+                    return this.$message.error("操作失敗!");
+                } 
+                this.$message.success("操作成功!");
+                this.updateDialogVisible = false;
+                this.getUserList();
+            })    
+        },
+        // 顯示權限設定對話框
+        async showSettingDialog(id){
+            const {data:res} = await this.$axios.get("http://localhost:9000/getUser?id="+id);
+            // console.log(res);
+            // console.log(res.role);        
+            this.settingForm = res; // 
+            // console.log('res.role:' , typeof res.role);//string
+            // console.log('超級管理員:' , typeof "超級管理員");//string
+           
+            if(this.settingForm.role === '超級管理員'){
+                this.settingForm.role = 0;
+            }else if(this.settingForm.role === '普通管理員'){
+                this.settingForm.role = 1;
+            }else{
+                this.settingForm.role = 2;
+            }
+            this.settingDialogVisible = true;           
+        },
+        // 關閉權限設定對話框, 表單內容重置
+        settingDialogClosed(){
+            this.$refs.settingFormRef.resetFields();
+        },
+        // 權限設定確認
+        userRoleChanged(){
+            console.log(this.settingForm);
+            if(this.settingForm.role == 0){
+                this.settingForm.role = "超級管理員";
+            }else if(this.settingForm.role == 1){
+                this.settingForm.role = "普通管理員";
+            }else{
+                this.settingForm.role = "普通用戶";
+            }
+            console.log(this.settingForm);
+
+            this.$refs.settingFormRef.validate(async valid => {
+                if(!valid) return;
+                // 發更新請求
+                const {data:res} = await this.$axios.put("http://localhost:9000/updateUser", this.settingForm);
+                if( res != "success"){              
+                    return this.$message.error("操作失敗!");
+                } 
+                this.$message.success("操作成功!");
+                this.settingDialogVisible = false;
+                this.getUserList();
+            })
+        }, 
+        //handleChange
+        handleChange(){
+            this.$forceUpdate();
+        },
+    }
+}    
 </script>
 <style lang="less" scoped>
 .el-breadcrumb {
