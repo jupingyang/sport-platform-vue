ae297bce2e17c795a4463af0cb25b32faa6f1e64
ping <jupingyang0@gmail.com>
2024-06-28 18:53:33 +0800

    commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 當發生錯誤時終止
+set -e
+
+# 建構 (build這裡做的就是幫我們把Vue轉成HTML/JS/CSS，並放入dist)
+npm run build
+
+# cd 到建構輸出的目錄下
+cd dist
+
+# 如果要部署到自定義域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A 
+git commit -m "deploy"
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jupingyang/sport-platform-vue.git main:gh-pages
+
+cd ..
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // include: /node_modules/,
-  // type: "javascript/auto"
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sport-platform-vue/'
+    : '/'
 })
